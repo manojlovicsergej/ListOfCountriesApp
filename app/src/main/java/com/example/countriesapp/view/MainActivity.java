67a3612c3c967cb0081b67132332e9d8755492b9
Sergej Manojlovic <manojlovics@outlook.com>
2023-02-21 18:38:41 +0100

    added model, view and viewmodel classes

--- app/src/main/java/com/example/countriesapp/view/MainActivity.java
+++ app/src/main/java/com/example/countriesapp/view/MainActivity.java
@@ -1,8 +1,10 @@
-package com.example.countriesapp;
+package com.example.countriesapp.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.countriesapp.R;
 
 public class MainActivity extends AppCompatActivity {
 
