82d250d07a8769d314226bbf456bcd7cd52031fe
Sergej Manojlovic <manojlovics@outlook.com>
2023-02-24 15:36:02 +0100

    added adapter

---
+++ app/src/main/java/com/example/countriesapp/view/CountryListAdapter.java
@@ -0,0 +1,65 @@
+package com.example.countriesapp.view;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.countriesapp.R;
+import com.example.countriesapp.model.CountriesModel;
+
+import java.util.List;
+
+import dagger.Binds;
+
+public class CountryListAdapter extends RecyclerView.Adapter<CountryListAdapter.CountryViewHolder> {
+
+    private List<CountriesModel> countries;
+
+    public CountryListAdapter(List<CountriesModel>countries){
+        this.countries = countries;
+    }
+
+    public void updateCountries(List<CountriesModel> newCountries){
+        countries.clear();
+        countries.addAll(newCountries);
+        notifyDataSetChanged();
+    }
+
+    @NonNull
+    @Override
+    public CountryViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_country,parent,false);
+        return new CountryViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull CountryViewHolder holder, int position) {
+        holder.capital.setText(countries.get(position).getCapital());
+        holder.name.setText(countries.get(position).getCountryName());
+    }
+
+    @Override
+    public int getItemCount() {
+        return countries.size();
+    }
+
+    public static class CountryViewHolder extends RecyclerView.ViewHolder{
+        private ImageView imageView;
+        private TextView name;
+        private TextView capital;
+
+        public CountryViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            imageView = itemView.findViewById(R.id.imageView);
+            name = itemView.findViewById(R.id.name);
+            capital = itemView.findViewById(R.id.capital);
+        }
+    }
+
+}
